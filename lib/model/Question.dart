e411ea485ec11a9f5bc6f7c96fc78ae5b11c311c
xxjonesriderxx <jonaslungruen@gmx.de>
2021-03-10 00:06:47 +0100

    Basics are working

--- lib/model/Question.dart
+++ lib/model/Question.dart
@@ -6,10 +6,20 @@ class Question {
   final String imagePath;
   final String question;
   final List<String> answers;
-  //index of right answer
-  final int solution;
 
-  const Question({this.imagePath, @required this.chapter, @required this.question, @required this.answers, @required this.solution, });
+  //index of right answer
+  final int solutionIndex;
+
+  //number from 1 to 15
+  final int difficulty;
+
+  const Question(
+      {this.imagePath,
+      @required this.chapter,
+      @required this.question,
+      @required this.answers,
+      @required this.solutionIndex,
+      @required this.difficulty});
 }
 
 enum Chapter{
@@ -18,6 +28,24 @@ enum Chapter{
 }
 
 const List<Question> allQuestions = [
-  Question(chapter: const [Chapter.neuesTestament], question: "Das ist eine Frage", answers: const ["falsch", "falsch", "richtig"], solution: 2),
-  Question(chapter: const [Chapter.neuesTestament], question: "Das ist eine weitere Frage", answers: const ["falsch", "richtig", "falsch"], solution: 1),
+  Question(
+      chapter: const [Chapter.neuesTestament],
+      question: "Das ist eine Frage",
+      answers: const [
+        "falsch",
+        "falsch",
+        "richtig",
+        "test1",
+        "test2",
+        "test3",
+        "test4"
+      ],
+      solutionIndex: 2,
+      difficulty: 1),
+  Question(
+      chapter: const [Chapter.neuesTestament],
+      question: "Das ist eine weitere Frage",
+      answers: const ["falsch", "richtig", "falsch"],
+      solutionIndex: 1,
+      difficulty: 3),
 ];
--- lib/ui/Quiz.dart
+++ lib/ui/Quiz.dart
@@ -2,17 +2,18 @@ import 'package:bible_quiz/model/Question.dart';
 import 'package:flutter/material.dart';
 
 class Quiz extends StatefulWidget{
-  static int currentQuestion = 0;
+  static int currentQuestion;
   final int numberOfQuestions;
   final Chapter chapter;
 
-  const Quiz({Key key, this.numberOfQuestions, this.chapter}) : super(key: key);
+  Quiz({Key key, this.numberOfQuestions, this.chapter}) : super(key: key) {
+    currentQuestion = 0;
+  }
 
   @override
   State<StatefulWidget> createState() {
     return _State();
   }
-
 }
 
 class _State extends State<Quiz>{
@@ -20,37 +21,55 @@ class _State extends State<Quiz>{
   final int numberOfQuestions;
   final Chapter chapter;
   int numberOfCards;
-  static const double _paddingBetweenQuestionAndAnswer = 32;
+  static const double _paddingBetweenQuestionAndAnswer = 8;
   static const double _paddingLeftRight = 32;
+  static const double _heightOfCard = 80;
+  bool _answered = false;
+  int _chosen;
+  List<Color> colorsOfButtons = [];
+  Color defaultColor = Colors.blue;
+  Color correctColor = Colors.green;
+  Color wrongColor = Colors.red;
+  Color solutionColor = Colors.green;
 
   _State({this.numberOfQuestions, this.chapter});
 
   @override
   void initState() {
-    if(chapter !=null){
-      allQuestions.forEach((question) {
-        if (question.chapter.contains(chapter)) {
-          if(numberOfQuestions!=null) {
-            if(questions.length<numberOfQuestions) {
+    if (questions.isEmpty) {
+      if (chapter != null) {
+        allQuestions.forEach((question) {
+          if (question.chapter.contains(chapter)) {
+            if (numberOfQuestions != null) {
+              if (questions.length < numberOfQuestions) {
+                questions.add(question);
+              }
+            } else {
               questions.add(question);
             }
-          }else{
-            questions.add(question);
           }
-        }
-      });
-    }else if(numberOfQuestions!=null){
-      allQuestions.forEach((question) {
-        if (question.chapter.contains(chapter)) {
-          if(questions.length<numberOfQuestions) {
-            questions.add(question);
+        });
+      } else if (numberOfQuestions != null) {
+        allQuestions.forEach((question) {
+          if (question.chapter.contains(chapter)) {
+            if (questions.length < numberOfQuestions) {
+              questions.add(question);
+            }
           }
-        }
-      });
-    }else{
-      questions.addAll(allQuestions);
+        });
+      } else {
+        questions.addAll(allQuestions);
+      }
     }
+    initColors();
     numberOfCards = 1 + questions[Quiz.currentQuestion].answers.length;
+  }
+
+  void initColors() {
+    colorsOfButtons = [];
+    for (int i = 0; i < questions[Quiz.currentQuestion].answers.length; i++) {
+      colorsOfButtons.add(defaultColor);
+    }
   }
 
   @override
@@ -58,61 +77,116 @@ class _State extends State<Quiz>{
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Frage ${Quiz.currentQuestion+1} von ${numberOfQuestions!=null?numberOfQuestions:allQuestions.length}"),
+        title: Text(
+            "Frage ${Quiz.currentQuestion + 1} von ${numberOfQuestions != null ? numberOfQuestions : allQuestions.length}"),
       ),
       body: Container(
         padding: EdgeInsets.only(top: 16),
         width: MediaQuery.of(context).size.width,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: _getQuestionContent(),
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _getQuestion(),
+            Padding(
+                padding:
+                    EdgeInsets.only(top: _paddingBetweenQuestionAndAnswer)),
+            Container(
+              //padding: EdgeInsets.only(left: _paddingLeftRight, right: _paddingLeftRight),
+              child: Expanded(
+                child: _getAnswers(),
+              ),
+            )
+          ],
         ),
       ),
+      floatingActionButton: _answered
+          ? FloatingActionButton(
+              child: Icon(Icons.arrow_forward_ios),
+              onPressed: () {
+                setState(() {
+                  if (Quiz.currentQuestion < questions.length - 1) {
+                    Quiz.currentQuestion++;
+                    initState();
+                    setState(() {
+                      _chosen = null;
+                      _answered = false;
+                    });
+                  } else {
+                    //show result
+                  }
+                });
+              },
+            )
+          : Container(),
     );
-  }
-
-  List<Widget> _getQuestionContent(){
-    List<Widget> allWidgets = [];
-    allWidgets.add(_getQuestion());
-    allWidgets.add(Padding(padding: EdgeInsets.only(top: _paddingBetweenQuestionAndAnswer),));
-    allWidgets.addAll(_getAnswers());
-    return allWidgets;
   }
 
   Widget _getQuestion(){
     return _getCard(questions[Quiz.currentQuestion].question, Theme.of(context).primaryColor, true);
   }
 
-  List<Widget> _getAnswers(){
+  Widget _getAnswers() {
     List<Widget> allAnswers = [];
-    questions[Quiz.currentQuestion].answers.forEach((answer) { 
-      allAnswers.add(_getCard(answer, Colors.blueGrey, false));
-    });
-    return allAnswers;
+    for (int i = 0; i < questions[Quiz.currentQuestion].answers.length; i++) {
+      allAnswers.add(_getCard(
+          questions[Quiz.currentQuestion].answers[i], Colors.blueGrey, false,
+          indexOfQuestion: i));
+    }
+    return SingleChildScrollView(
+      child: Column(
+        children: allAnswers,
+      ),
+    );
   }
-  
-  Card _getCard(String text, Color color, bool question){
-    debugPrint("number of cards: "+numberOfCards.toString());
+
+  Card _getCard(String text, Color color, bool question,
+      {int indexOfQuestion}) {
+    debugPrint("number of cards: " + numberOfCards.toString());
     return Card(
       color: color,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(6.0),
       ),
-      child: question?Container(
-        alignment: Alignment.center,
-        width: MediaQuery.of(context).size.width-_paddingLeftRight,
-        padding: EdgeInsets.all(16),
-        child: Text(text),
-      ):ElevatedButton(
-        style: ButtonStyle(),
-          child: Container(
-            alignment: Alignment.center,
-            width: MediaQuery.of(context).size.width-_paddingLeftRight,
-            padding: EdgeInsets.all(16),
-            child: Text(text),
-          )
-      ),
+      child: question
+          ? Container(
+              //question container
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width - _paddingLeftRight,
+              height: _heightOfCard,
+              padding: EdgeInsets.all(16),
+              child: Text(text),
+            ):ElevatedButton(
+              //answer button
+              style: ElevatedButton.styleFrom(
+                primary: colorsOfButtons[indexOfQuestion], // background
+                onPrimary: Colors.black, // foreground
+              ),
+              child: Container(
+                color: colorsOfButtons[indexOfQuestion],
+                alignment: Alignment.center,
+                width:
+                    MediaQuery.of(context).size.width - _paddingLeftRight * 2,
+                height: _heightOfCard,
+                padding: EdgeInsets.all(16),
+                child: Text(text),
+              ),
+              onPressed: () {
+                setState(() {
+                  if (!_answered) {
+                    if (questions[Quiz.currentQuestion].solutionIndex ==
+                        indexOfQuestion) {
+                      colorsOfButtons[indexOfQuestion] = correctColor;
+                    } else {
+                      colorsOfButtons[indexOfQuestion] = wrongColor;
+                      colorsOfButtons[questions[Quiz.currentQuestion]
+                          .solutionIndex] = solutionColor;
+                    }
+                    _chosen = indexOfQuestion;
+                    _answered = true;
+                  }
+                });
+              },
+            ),
     );
   }
-
 }
